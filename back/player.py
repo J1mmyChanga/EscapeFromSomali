5d92d9a3ea3ac0ff1ce3ece0f99c0541894a22ec
Vitalik <epiakovmartynenkovv@yandex.ru>
2023-01-08 21:27:39 +0300

    +norm jumping starting to load images

--- back/player.py
+++ back/player.py
@@ -5,35 +5,73 @@ from support import import_folder
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(all_sprites, group)
-        self.image = pygame.Surface((32, 64))
-        self.image.fill(pygame.Color('yellow'))
+        self.import_character_images()
+        self.frame_index = 0
+        self.animation_speed = 0.01
+        self.image = self.animations['idle'][self.frame_index]
         self.rect = self.image.get_rect(topleft = pos)
 
         self.direction = pygame.math.Vector2(0, 0)
         self.gravity = 0.8
         self.jump_speed = -16
         self.speed = 8
-        self.on_ground = True
+        self.on_ground = False
+        self.on_ceiling = False
+        self.on_left = False
+        self.on_right = False
+        self.status = 'idle'
+        self.facing_right = True
 
     def import_character_images(self):
-        character_path = 'path'
+        character_path = './front/character/'
         self.animations = {'idle':[], 'run':[], 'jump':[], 'fall':[]}
         for animation in self.animations.keys():
             full_path = character_path + animation
             self.animations[animation] = import_folder(full_path)
+
+    def animate(self):
+        animation = self.animations[self.status]
+        self.frame_index = (self.frame_index + self.animation_speed) % len(animation)
+
+        image = animation[int(self.frame_index)]
+        if self.facing_right:
+            self.image = image
+        else:
+            self.image = pygame.transform.flip(image, True, False)
+
+        if self.on_ground:
+            self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
+        elif self.on_ceiling:
+            self.rect = self.image.get_rect(midtop = self.rect.midtop)
+        else:
+            self.rect = self.image.get_rect(center = self.rect.center)
+
 
     def movement(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_d]:
             self.direction.x = 1
+            self.facing_right = True
         elif keys[pygame.K_a]:
             self.direction.x = -1
+            self.facing_right = False
         else:
             self.direction.x = 0
         if keys[pygame.K_SPACE] and self.on_ground:
             self.jump()
             self.on_ground = False
+
+    def get_status(self):
+        if self.direction.y < 0:
+            self.status = 'jump'
+        elif self.direction.y > 1:
+            self.status = 'fall'
+        else:
+            if self.direction != 0:
+                self.status = 'running'
+            else:
+                self.status = 'idle'
 
     def apply_gravity(self):
         self.direction.y += self.gravity
@@ -44,3 +82,5 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         self.movement()
+        # self.get_status()
+        self.animate()
--- back/support.py
+++ back/support.py
@@ -1,5 +1,24 @@
-from os import walk
+import pygame, sys, os
 
 def import_folder(path):
-    for _, __, img_file in walk(path):
-         print(path + img_file)
+    surface_list = []
+    for _, __, img_files in os.walk(path):
+        for image in img_files:
+            full_path = f'{path}/{image}'
+            img_smurf = load_image(full_path)
+            surface_list.append(img_smurf)
+    return surface_list
+
+def load_image(name, colorkey=None):
+    if not os.path.isfile(name):
+        print(f"Файл с изображением '{name}' не найден")
+        sys.exit()
+    image = pygame.image.load(name)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
