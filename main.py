587c9e890cdfe2a6e6a496d740e2e362b28b98e8
Vitalik <epiakovmartynenkovv@yandex.ru>
2023-01-08 17:02:29 +0300

    +main +settings +tiles +level

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+from settings import *
+from level import Level
+
+
+pygame.init()
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+level = Level(level_map, screen)
+running = True
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+while running:
+    screen.fill((0, 0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    level.run()
+    pygame.display.flip()
+    clock.tick(60)
+terminate()
---
+++ tiles.py
@@ -0,0 +1,8 @@
+from settings import *
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, pos, size, group):
+        super().__init__(all_sprites, group)
+        self.image = pygame.Surface((size, size))
+        self.image.fill(pygame.Color('grey'))
+        self.rect = self.image.get_rect(topleft = pos)
