0af066010678c4cb2758f515cadbb3367001a99a
Vitalik <epiakovmartynenkovv@yandex.ru>
2023-01-09 17:20:11 +0300

    +particles but without runnig image

--- back/level.py
+++ back/level.py
@@ -2,6 +2,7 @@ import pygame
 from tiles import Tile
 from settings import tile_size, all_sprites, screen_width
 from player import Player
+from particle import ParticleEffect
 
 
 class Level:
@@ -9,6 +10,10 @@ class Level:
         self.display_surface = surface
         self.world_shift = 0
         self.setup_level(level_data)
+
+        #частицы
+        self.dust_sprite = pygame.sprite.GroupSingle()
+        self.player_on_ground = False
 
     def setup_level(self, layout):                  #создание тайлов и игрока(отрисовка в классах)
         self.tiles = pygame.sprite.Group()
@@ -20,7 +25,30 @@ class Level:
                 if col == '#':
                     tile = Tile((x, y), tile_size, self.tiles)
                 elif col == 'P':
-                    player = Player((x, y), self.player)
+                    player = Player((x, y), self.player, self.display_surface, self.create_jump_particles)
+
+    def create_jump_particles(self, pos):
+        if self.player.sprite.facing_right:
+            pos -= pygame.math.Vector2(10, -5)
+        else:
+            pos += pygame.math.Vector2(10, 5)
+        jump_particle_effect = ParticleEffect(pos, 'jump')
+        self.dust_sprite.add(jump_particle_effect)
+
+    def get_player_sprite_on_ground(self):
+        if self.player.sprite.on_ground:
+            self.player_on_ground = True
+        else:
+            self.player_on_ground = False
+
+    def create_landing_dust(self):
+        if not self.player_on_ground and self.player.sprite.on_ground and not self.dust_sprite.sprites():
+            if self.player.sprite.facing_right:
+                offset = pygame.math.Vector2(10, 15)
+            else:
+                offset = pygame.math.Vector2(-10, 15)
+            fall_dust_particle = ParticleEffect(self.player.sprite.rect.midbottom, 'land')
+            self.dust_sprite.add(fall_dust_particle)
 
     def camera_movement(self):                     #камера(двигается левел при достижении игроком краев экрана)
         player = self.player.sprite
@@ -76,11 +104,16 @@ class Level:
             player.on_ceiling = False
 
     def run(self):
+        self.dust_sprite.update(self.world_shift)
+        self.dust_sprite.draw(self.display_surface)
+
         self.camera_movement()                       #движение камеры(переопределение скорости движения игрока и уровня)
         self.tiles.update(self.world_shift)          #смещение уровня по вышепереопределенным переменным
         self.tiles.draw(self.display_surface)        #отрисовка
 
         self.player.update()                         #метод игрока
-        self.horizontal_collision()                  #проверка на столкновения по вертикали и горизонтали и соответствующее изменение флагов
-        self.vertical_collision()
+        self.horizontal_collision()
+        self.get_player_sprite_on_ground()
+        self.vertical_collision()                      #проверка на столкновения по вертикали и горизонтали и соответствующее изменение флагов
+        self.create_landing_dust()
         self.player.draw(self.display_surface)       #отрисовка
---
+++ back/particle.py
@@ -0,0 +1,26 @@
+import pygame
+from support import import_folder
+from settings import all_sprites
+
+class ParticleEffect(pygame.sprite.Sprite):
+    def __init__(self, pos, type):
+        super().__init__(all_sprites)
+        self.frame_index = 0
+        self.animation_speed = 0.5
+        if type == 'jump':
+            self.frames = import_folder('./front/dust_particles/jump')
+        elif type == 'fall':
+            self.frames = import_folder('./front/dust_particles/land')
+        self.image = self.frames[self.frame_index]
+        self.rect = self.image.get_rect(center = pos)
+
+    def animate(self):
+        self.frame_index += self.animation_speed
+        if self.frame_index >= len(self.frames):
+            self.kill()
+        else:
+            self.image = self.frames[int(self.frame_index)]
+
+    def update(self, dx):
+        self.animate()
+        self.rect.x += dx
