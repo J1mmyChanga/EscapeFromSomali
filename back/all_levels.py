77545220f0f1951160a52385c44060d8a3176784
cosm0oo <happy_doge@bk.ru>
2023-01-20 00:06:12 +0300

    added levels 3, 4, 5

--- back/all_levels.py
+++ back/all_levels.py
@@ -19,3 +19,36 @@ level_2 = {
     'ground': '../levels/level_2/level_2_ground.csv',
     'obstacles': '../levels/level_2/level_2_obstacles.csv',
     'player': '../levels/level_2/level_2_player.csv',}
+
+level_3 = {
+    'background palms': '../levels/level_3/level_3_background_palms.csv',
+    'consumables': '../levels/level_3/level_3_consumables.csv',
+    'crates': '../levels/level_3/level_3_crates.csv',
+    'enemies': '../levels/level_3/level_3_enemies.csv',
+    'foreground palms': '../levels/level_3/level_3_foreground_palms.csv',
+    'grass': '../levels/level_3/level_3_grass.csv',
+    'ground': '../levels/level_3/level_3_ground.csv',
+    'obstacles': '../levels/level_3/level_3_obstacles.csv',
+    'player': '../levels/level_3/level_3_player.csv',}
+
+level_4 = {
+    'background palms': '../levels/level_4/level_4_background_palms.csv',
+    'consumables': '../levels/level_4/level_4_consumables.csv',
+    'crates': '../levels/level_4/level_4_crates.csv',
+    'enemies': '../levels/level_4/level_4_enemies.csv',
+    'foreground palms': '../levels/level_4/level_4_foreground_palms.csv',
+    'grass': '../levels/level_4/level_4_grass.csv',
+    'ground': '../levels/level_4/level_4_ground.csv',
+    'obstacles': '../levels/level_4/level_4_obstacles.csv',
+    'player': '../levels/level_4/level_4_player.csv',}
+
+level_5 = {
+    'background palms': '../levels/level_5/level_5_background_palms.csv',
+    'consumables': '../levels/level_5/level_5_consumables.csv',
+    'crates': '../levels/level_5/level_5_crates.csv',
+    'enemies': '../levels/level_5/level_5_enemies.csv',
+    'foreground palms': '../levels/level_5/level_5_foreground_palms.csv',
+    'grass': '../levels/level_5/level_5_grass.csv',
+    'ground': '../levels/level_5/level_5_ground.csv',
+    'obstacles': '../levels/level_5/level_5_obstacles.csv',
+    'player': '../levels/level_5/level_5_player.csv',}
